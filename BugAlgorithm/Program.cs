35f9a3a986fc16334f8ec6d6ac92799ee28152dd
yash-15 <yash.15296@gmail.com>
2016-09-18 05:04:46 +0530

    Version 1.0.0 !!! Stable Version Finally.

--- BugAlgorithm/Program.cs
+++ BugAlgorithm/Program.cs
@@ -8,76 +8,76 @@ using System.Data;
 
 public partial class Form1 : System.Windows.Forms.Form
 {
-    private List< Point[] > obstacles;
-    private Point pos, delta, start, end;
-    private PointF posF, velF, checkpoint;
-    private double theta, theta0;
+    private bool debug = false;
+    private List<Point[]> obstacles;
+    Dictionary<Point[], Line[]> edges;
+    private Point pos,end;
+    private PointF start, endF, posF, velF, checkpoint,target;
+    private double theta,pathLength=0;
     private int moveMode;
     private enum moveType { Free, Boundary};
-    private float speed = 5.0F;
+    private float speed;
+    private int bugRadius;
     private Point[] contact;
     private int index;
+    private const float eps = 1.5F,del = 0.001F;
+    int frameCount = 0;
     private void InitializeAlgorithm()
     {
+        this.bugRadius = 10;
+        this.speed = 0.9F;
         this.obstacles = new List<Point[]>();
+        this.edges = new Dictionary<Point[],Line[]>();
         this.obstaclesUI = new List<Point[]>(); 
         readFromFile();
-        start = new Point(300, 100);
-        end = new Point(300, 400);
-        theta0 = Math.Atan2(end.Y - start.Y, end.X - start.X);
-        posF = pos = start;
+        start = new Point(50, 50);
+        endF = new Point(450, 400);
+        posF = start;
+        pos.X = (int)posF.X;    pos.Y = (int)posF.Y;
+        end.X = (int)endF.X;    end.Y = (int)endF.Y;
+
         moveMode = (int)moveType.Free;
         checkpoint = start;
     }
-
     
     private void timer1_Tick(object sender, System.EventArgs e)
     {
+        theta = Math.Atan2(target.Y - posF.Y, target.X - posF.X);
+        velF.X = speed * (float)Math.Cos(theta);
+        velF.Y = speed * (float)Math.Sin(theta);
+        posF.X += velF.X;
+        posF.Y += velF.Y;
+        pos.X = (int)posF.X;
+        pos.Y = (int)posF.Y;
+        pathLength += speed;
         if (moveMode == (int)moveType.Free)
         {
-            velF.X = speed * (float)Math.Cos(theta0);
-            velF.Y = speed * (float)Math.Sin(theta0);
-            posF.X += velF.X;
-            posF.Y += velF.Y;
-            pos.X = (int)posF.X;
-            pos.Y = (int)posF.Y;
-
-            obstacleDetection1();
+            obstacleDetection();
         }
         else
         {
-            theta = Math.Atan2(contact[index].Y - posF.Y, contact[index].X - posF.X);
-            velF.X = speed * (float)Math.Cos(theta);
-            velF.Y = speed * (float)Math.Sin(theta);
-            posF.X += velF.X;
-            posF.Y += velF.Y;
-            pos.X = (int)posF.X;
-            pos.Y = (int)posF.Y;
-
-            //TODO: boundary case translation
+            if (reached(posF, target))
+            {
+                pathLength += distance(posF, target);
+                posF = target;
+                this.clickFrame.Invalidate(true);
+                obstacleDetection();
+            }
         }
 
-        if (reached(posF, end))
+        if (reached(posF, endF))
+        {
+            pathLength += distance(posF, end);
+            posF = endF;
+            pos.X = (int)posF.X;
+            pos.Y = (int)posF.Y;
             timer1.Stop();
+            this.txtLen.Font = new Font(txtLen.Font, FontStyle.Bold);
+        }
 
-       /* int newX, newY;
-       /* newX = picBall.Location.X + dx;
-        newY = picBall.Location.Y + dy;
-
-        if (newX > this.ClientSize.Width - picBall.Width)
-            dx = -dx;
-        if (newX < 0)
-            dx = -dx;
-        if (newY > this.ClientSize.Height - picBall.Height)
-            dy = -dy;
-        if (newY < 0)
-            dy = -dy;
-
-        picBall.Location = new Point(newX, newY);
-        */
-        this.txtX.Text = pos.X.ToString();
-        this.txtY.Text = pos.Y.ToString();
-        this.comment.Text = moveMode.ToString();
+        this.txtX.Text = "X: "+posF.X.ToString("0.00");
+        this.txtY.Text = "Y: "+posF.Y.ToString("0.00");
+        this.txtLen.Text = "Path Length: " + pathLength.ToString("0.00");
         this.clickFrame.Invalidate(true);
     }
 
@@ -86,57 +86,220 @@ public partial class Form1 : System.Windows.Forms.Form
         public float a, b, c;
         public Line(float x, float y, float z)
         {
-            a = x;
-            b = y;
-            c = z;
+            this.a = x;
+            this.b = y;
+            this.c = z;
+        }
+        public Line(PointF a, PointF b)
+        {
+            this.a = a.Y - b.Y;
+            this.b = b.X - a.X;
+            this.c = a.X * b.Y - b.X * a.Y;
         }
     };
 
-    private Line twoPointLine(PointF a, PointF b)
+    bool isParallel(Line l1, Line l2)
     {
-        Line l;
-        l.a = a.Y - b.Y;
-        l.b = b.X - a.X;
-        l.c=  a.X*b.Y-b.X*a.Y;
-        return l;
+        return (l1.a * l2.b == l1.b * l2.a);
     }
 
-    private bool isCollinear(PointF a, PointF b, PointF c)
+    PointF intersect(Line l1, Line l2)
     {
-        return false;
-        b.X-=a.X;   b.Y-=a.Y;
-        c.X-=a.X;   c.Y-=a.Y;
-
-        if (b.X*c.X<0 || b.Y*c.Y<0)
-                return false;
-
-        double dist = Math.Abs(b.Y * c.X - b.X - c.Y)/Math.Sqrt(c.X*c.X+c.Y*c.Y);
-
-        return (dist < (double)bugRadius);
+        PointF x=new PointF(0,0);
+        x.X = (l1.b * l2.c - l2.b * l1.c) / (l1.a * l2.b - l2.a * l1.b);
+        x.Y = (l1.a * l2.c - l2.a * l1.c) / (l1.b * l2.a - l2.b * l1.a);
+        return x;
     }
 
-    private void obstacleDetection1()
+    PointF footOfPerp(Line l, PointF p)
+    {
+        PointF fop = new PointF();
+        fop.X = p.X - ((l.a * (l.a * p.X + l.b * p.Y + l.c)) / (l.a * l.a + l.b * l.b));
+        fop.Y = p.Y - ((l.b * (l.a * p.X + l.b * p.Y + l.c)) / (l.a * l.a + l.b * l.b));
+        return fop;
+    }
+
+    float distance(PointF a, PointF b)
+    {
+        float x1=a.X-b.X, x2=a.Y-b.Y;
+        return (float)Math.Sqrt(x1 * x1 + x2 * x2);
+    }
+
+    bool inOrder(PointF a, PointF b, PointF c, int offset)
+    {
+        if (distance(a, b) > (offset + distance(a, c)))
+            return false;
+        if (distance(c, b) > (offset + distance(a, c)))
+            return false;
+        return true;
+    }
+
+    PointF xPt;
+
+    private bool isAligned(PointF a, PointF b, PointF c, Line l1, Line l2)
+    {
+       // Console.Write("{0}x+{1}y+{2}=0  {3}x+{4}y+{5}=0  ", l1.a, l1.b, l1.c, l2.a, l2.b, l2.c);
+        if (isParallel(l1, l2))
+            return false;
+        xPt = intersect(l1, l2);
+        if(debug)
+            Console.WriteLine(" {0} {1}  {2} {3}  {4} {5}  {6} {7} ", a.X, a.Y, c.X, c.Y, b.X, b.Y, xPt.X, xPt.Y); 
+        if (!inOrder(a, xPt, c, 0))
+            return false;
+        if (!inOrder(b, xPt, end, 0))
+            return false;
+        if (distance(xPt, b) > 2 * eps)
+            return false;
+        if (distance(xPt, c) < eps)
+            return false;
+        return true;
+    }
+
+    private void obstacleDetection()
+    {
+        target = end;
+        Line toOrigin = new Line(posF, end);
+        foreach (Point[] obs in obstacles)
+        {
+            Line[] edg = edges[obs];
+            for (int i = 0; i < edg.Length; i++){
+            //    Console.WriteLine(" ");
+            //    Console.WriteLine(" {6} :: {0} {1}  {2} {3}  {4} {5} ", obs[i].X, obs[i].Y, obs[i+1].X, obs[i+1].Y, posF.X, posF.Y,i);
+                if (!isAligned(obs[i], posF, obs[i+1],edg[i],toOrigin))
+                    continue;
+                if (distance(xPt,posF)<eps)
+                    if (!PointinPoly(obs, extendedPoint(xPt, end, -2*eps)))
+                        continue;
+                if(debug)
+                    Console.WriteLine("Intersection found!");
+                contact = obs;
+                index = i+1;
+                moveMode = (int)moveType.Boundary;
+                checkpoint = posF;
+                pathLength += distance(posF, xPt);
+                posF = xPt;
+                target = obs[i+1];
+                return;
+            }
+        }
+        moveMode = (int)moveType.Free;
+    }
+
+    bool reached(PointF a, PointF b)
+    {
+        a.X -= b.X;
+        a.Y -= b.Y;
+        double dist = Math.Sqrt(a.X * a.X + a.Y * a.Y);
+        if (dist > eps)
+            return false;
+        if (a.X * velF.X + a.Y * velF.Y >= 0)
+            return true;
+        else
+            return false;
+    }
+
+    private bool isAligned_withRadius(PointF a, PointF b, PointF c)
+    {
+    //    System.Console.WriteLine("!!! a = {0},{1}  b = {2},{3} c = {4},{5}", a.X, a.Y, b.X, b.Y, c.X, c.Y);
+        Line l1,l2;
+        l1 = new Line(a,c);
+        l2 = new Line(b,endF);
+        PointF tmp1 = footOfPerp(l1, b);
+        if (distance(tmp1, b) < bugRadius && inOrder(a,b,c,0))
+        {
+            Console.Write(" correction: {0} {1}", posF.X, posF.Y);
+            posF = extendedPoint(tmp1, b, -bugRadius*1.05F);
+            //noContact = false;
+            Console.WriteLine(" : {0} {1}", posF.X, posF.Y);
+        //    return false;
+        }
+        if (isParallel(l1, l2))
+            return false;
+        xPt = intersect(l1, l2);
+      //  System.Console.WriteLine(" {0},{1} ", pt.X, pt.Y);
+        if(!inOrder(a,xPt,c,bugRadius))
+        if(distance(b,endF) > (bugRadius+distance(xPt,endF)))
+            return false;
+        if(distance(b,endF) < distance(xPt,endF))
+            return false;
+   //     System.Console.WriteLine("!!! a = {0},{1}  b = {2},{3} c = {4},{5}",a.X,a.Y,b.X,b.Y,c.X,c.Y);
+        return true;
+    }
+
+    private void obstacleDetection_withRadius()
     {
         foreach (Point[] obs in obstacles)
         {
             for (int i = 1; i < obs.Length; i++)
-                if (isCollinear(obs[i - 1], posF, obs[i]))
+                if (isAligned_withRadius(obs[i - 1], posF, obs[i]))
                 {
                     contact = obs;
                     index = i;
                     moveMode = (int)moveType.Boundary;
                     checkpoint = posF;
+                    posF = extendedPoint(xPt, end, bugRadius);
+                    Console.WriteLine("{0} {1} :: {2} {3}", xPt.X, xPt.Y, posF.X, posF.Y);
+                    target = similarPoint(extendedPoint(obs[i], xPt, bugRadius * 1.1F));
+                    //     timer1.Stop();
                     return;
                 }
         }
+        moveMode = (int)moveType.Free;
     }
 
-    bool reached(PointF a, PointF b)
+    PointF similarPoint(PointF a)
+    {
+        return new PointF(a.X + pos.X - xPt.X,a.Y + pos.Y - xPt.Y);
+    }
+
+    PointF extendedPoint(PointF a, PointF b, float dist)
+    {
+        double tht = Math.Atan2(a.Y - b.Y, a.X - b.X);
+        dist += distance(a, b);
+        return new PointF(b.X+dist*(float)Math.Cos(tht),b.Y+dist*(float)Math.Sin(tht));
+    }
+    
+    bool reached_withRadius(PointF a, PointF b)
     {
         a.X-=b.X;
         a.Y-=b.Y;
         double dist = Math.Sqrt(a.X * a.X + a.Y * a.Y);
-        return (dist < (double)bugRadius);
+        if (dist > (double)bugRadius)
+            return false;
+        if (a.X * velF.X + a.Y * velF.Y < 0)
+            return true;
+        else
+            return false;
+    }
+
+    private bool PointinPoly(Point[] obs, PointF pt) 
+    {
+        Console.WriteLine("Checking {0} {1}", pt.X, pt.Y);
+        bool ans = false;
+        for (int i = 1; i < obs.Length; i++)
+        {
+            if (!inOrder(obs[i - 1].Y, pt.Y, obs[i].Y))
+                continue;
+            if (obs[i - 1].X > pt.X && obs[i].X > pt.X)
+                continue;
+            if (!(obs[i - 1].X <= pt.X && obs[i].X <= pt.X))
+            {
+                float x3=obs[i].X+(obs[i].X-obs[i-1].X)*(pt.Y-obs[i].Y)/(obs[i].Y-obs[i-1].Y);
+                if (x3 >= pt.X)
+                    continue;
+            }
+            ans ^= true;
+        }
+        return ans;
+    }
+
+    bool inOrder(float a, float b, float c)
+    {
+        if (a <= b && b <= c)
+            return true;
+        if (a >= b && b >= c)
+            return true;
+        return false;
     }
 
     private void readFromFile()
@@ -144,14 +307,17 @@ public partial class Form1 : System.Windows.Forms.Form
         System.IO.StreamReader scan = new System.IO.StreamReader("Resources/obstacles.txt");
         string line = scan.ReadLine();
         string[] str;
-        Point[] poly;
+        Point[] poly,polyUI;
+        Line[] edg;
         int obstacleCount = Convert.ToInt32(line), pts;
+        Console.WriteLine("Obstacles count: {0}", obstacleCount);
         for (int i = 0; i < obstacleCount; i++)
         {
             line = scan.ReadLine();
             pts = Convert.ToInt32(line);
             poly = new Point[pts + 1];
-
+            polyUI = new Point[pts + 1];
+            edg = new Line[pts];
             for (int j = 0; j < pts; j++)
             {
                 str = scan.ReadLine().Split();
@@ -162,10 +328,16 @@ public partial class Form1 : System.Windows.Forms.Form
 
             for (int j = 0; j <= pts; j++)
             {
-                poly[j].X = X(poly[j].X);
-                poly[j].Y = Y(poly[j].Y);
+                polyUI[j].X = X(poly[j].X);
+                polyUI[j].Y = Y(poly[j].Y);
             }
-            obstaclesUI.Add(poly);
+            obstaclesUI.Add(polyUI);
+
+            for (int j = 0; j < pts; j++)
+            {
+                edg[j] = new Line(poly[j], poly[j + 1]);
+            }
+            edges.Add(poly,edg);
         }
         scan.Close();
     }
--- BugAlgorithm/UI.cs
+++ BugAlgorithm/UI.cs
@@ -12,17 +12,19 @@ public partial class Form1 : System.Windows.Forms.Form
 
     private System.Windows.Forms.PictureBox clickFrame;
     private System.Windows.Forms.Timer timer1;
-    private System.Windows.Forms.TextBox txtX;
-    private System.Windows.Forms.TextBox txtY;
-    private System.Windows.Forms.TextBox comment;
+    private System.Windows.Forms.Label txtX;
+    private System.Windows.Forms.Label txtY;
+    private System.Windows.Forms.Label txtLen;
+    private System.Windows.Forms.Label txtTitle;
     private System.Windows.Forms.Button newObjBtn;
+    private System.Windows.Forms.Button StartPosBtn;
+    private System.Windows.Forms.Button EndPosBtn;
     private System.Windows.Forms.Button pauseBtn;
     private List<Point[]> obstaclesUI;
     private System.Drawing.SolidBrush obsBrush;
     private System.Drawing.SolidBrush bugBrush;
     private System.Drawing.SolidBrush endBrush;
     private int clickMode;
-    private int bugRadius = 5;
     private enum clickType {NONE,OBS,INITIAL,FINAL};
 
     public Form1()
@@ -41,10 +43,13 @@ public partial class Form1 : System.Windows.Forms.Form
             this.components = new System.ComponentModel.Container();
             this.clickFrame = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.txtX = new System.Windows.Forms.TextBox();
-            this.txtY = new System.Windows.Forms.TextBox();
-            this.comment = new System.Windows.Forms.TextBox();
+            this.txtX = new System.Windows.Forms.Label();
+            this.txtY = new System.Windows.Forms.Label();
+            this.txtLen = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.Label();
             this.newObjBtn = new System.Windows.Forms.Button();
+            this.StartPosBtn = new System.Windows.Forms.Button();
+            this.EndPosBtn = new System.Windows.Forms.Button();
             this.pauseBtn = new System.Windows.Forms.Button();
             this.clickMode = (int)clickType.NONE;
 
@@ -53,71 +58,102 @@ public partial class Form1 : System.Windows.Forms.Form
             // 
             // clickFrame
             // 
-            this.clickFrame.Location = new System.Drawing.Point(150, 50);
+            this.clickFrame.Location = new System.Drawing.Point(200, 50);
             this.clickFrame.Name = "clickFrame";
-            this.clickFrame.Size = new System.Drawing.Size(600, 500);
+            this.clickFrame.Size = new System.Drawing.Size(505, 505);
             this.clickFrame.TabIndex = 0;
             this.clickFrame.TabStop = false;
             this.clickFrame.Click += new System.EventHandler(this.clickFrame_Click);
             // 
             // timer1
             // 
-            this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            this.timer1.Interval = 5;
             // 
             // txtX
             // 
-            this.txtX.Location = new System.Drawing.Point(25, 100);
+            this.txtX.Location = new System.Drawing.Point(25, 320);
             this.txtX.Name = "txtX";
-            this.txtX.Size = new System.Drawing.Size(100, 20);
-            this.txtX.TabIndex = 2;
+            this.txtX.Size = new System.Drawing.Size(100, 30);
+            this.txtX.Font = new Font(this.txtX.Font.Name, 11);
+            this.txtX.Text = "X: " + posF.X.ToString("0.00");
             // 
             // txtY
             // 
-            this.txtY.Location = new System.Drawing.Point(25, 150);
+            this.txtY.Location = new System.Drawing.Point(25, 350);
             this.txtY.Name = "txtY";
-            this.txtY.Size = new System.Drawing.Size(100, 20);
-            this.txtY.TabIndex = 3;
+            this.txtY.Size = new System.Drawing.Size(100, 30);
+            this.txtY.Font = new Font(this.txtY.Font.Name, 11);
+            this.txtY.Text = "Y: " + posF.Y.ToString("0.00");
+            //
+            // txtLen
             // 
-            // comment
+            this.txtLen.Location = new System.Drawing.Point(25, 400);
+            this.txtLen.Name = "txtLen";
+            this.txtLen.Size = new System.Drawing.Size(275, 30);
+            this.txtLen.Font = new Font(this.txtLen.Font.Name, 11);
+            this.txtLen.Text = "Path Length: " + pathLength.ToString("0.00");//
+            // txtTitle
             // 
-            this.comment.Location = new System.Drawing.Point(25, 200);
-            this.comment.Name = "comment";
-            this.comment.Size = new System.Drawing.Size(100, 20);
-            this.comment.TabIndex = 4;
+            this.txtTitle.Location = new System.Drawing.Point(325, 10);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(300, 60);
+            this.txtTitle.Font = new Font(this.txtLen.Font.Name, 15);
+            this.txtTitle.Text = "Bug Algorithm Simulation";
             // 
             // newObjBtn
             // 
-            this.newObjBtn.Location = new System.Drawing.Point(25, 50);
+            this.newObjBtn.Location = new System.Drawing.Point(25, 250);
             this.newObjBtn.Name = "newObjBtn";
             this.newObjBtn.Size = new System.Drawing.Size(100, 23);
-            this.newObjBtn.TabIndex = 0;
+            this.newObjBtn.TabIndex = 3;
             this.newObjBtn.Text = "Add obstacle";
             this.newObjBtn.Click += new System.EventHandler(this.newObj_Click);
             // 
+            // startPosBtn
+            // 
+            this.StartPosBtn.Location = new System.Drawing.Point(25, 150);
+            this.StartPosBtn.Name = "startPosBtn";
+            this.StartPosBtn.Size = new System.Drawing.Size(100, 23);
+            this.StartPosBtn.TabIndex = 1;
+            this.StartPosBtn.Text = "Set Start Position";
+            this.StartPosBtn.Click += new System.EventHandler(this.startPos_Click);
+            // 
+            // endPosBtn
+            // 
+            this.EndPosBtn.Location = new System.Drawing.Point(25, 200);
+            this.EndPosBtn.Name = "endPosBtn";
+            this.EndPosBtn.Size = new System.Drawing.Size(100, 23);
+            this.EndPosBtn.TabIndex = 2;
+            this.EndPosBtn.Text = "Set Final Position";
+            this.EndPosBtn.Click += new System.EventHandler(this.endPos_Click);
+            // 
             // pauseBtn
             // 
-            this.pauseBtn.Location = new System.Drawing.Point(25, 250);
+            this.pauseBtn.Location = new System.Drawing.Point(25, 100);
             this.pauseBtn.Name = "newObjBtn";
             this.pauseBtn.Size = new System.Drawing.Size(100, 23);
             this.pauseBtn.TabIndex = 0;
-            this.pauseBtn.Text = "Pause";
+            this.pauseBtn.Text = "Start";
             this.pauseBtn.Click += new System.EventHandler(this.pause_Click);
             // 
             // Form1
             // 
             this.BackColor = System.Drawing.Color.White;
             this.StartPosition = FormStartPosition.WindowsDefaultLocation;
-            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.ClientSize = new System.Drawing.Size(800, 650);
             this.clickFrame.Paint+=new PaintEventHandler(this.drawObstacles);
             this.Controls.Add(this.clickFrame);
             this.Controls.Add(this.txtX);
             this.Controls.Add(this.txtY);
-            this.Controls.Add(this.comment);
+            this.Controls.Add(this.txtLen);
+            this.Controls.Add(this.txtTitle);
             this.Controls.Add(this.newObjBtn);
+            this.Controls.Add(this.StartPosBtn);
+            this.Controls.Add(this.EndPosBtn);
             this.Controls.Add(this.pauseBtn);
             this.Name = "Form1";
-            this.Text = "Path planning: Bug Algorithm";
+            this.Text = "Motion planning: Bug Algorithm";
             ((System.ComponentModel.ISupportInitialize)(this.clickFrame)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -134,20 +170,40 @@ public partial class Form1 : System.Windows.Forms.Form
     }
     private int Y(int y)
     {
-        return  450-y;
+        return  500-y;
     }
     
     private void drawObstacles(object sender, PaintEventArgs e)
     {
-        foreach(Point[] obstacle in obstaclesUI)
-            e.Graphics.FillPolygon(obsBrush, obstacle);
-        e.Graphics.FillEllipse(bugBrush, X(pos.X - bugRadius), Y(pos.Y - bugRadius), 2 * bugRadius, 2 * bugRadius);
-        e.Graphics.FillEllipse(endBrush, X(end.X - bugRadius), Y(end.Y - bugRadius), 2 * bugRadius, 2 * bugRadius);
+        e.Graphics.FillEllipse(endBrush, X(end.X - bugRadius), Y(end.Y + bugRadius), 2 * bugRadius, 2 * bugRadius);
+        e.Graphics.FillEllipse(bugBrush, X(pos.X - bugRadius), Y(pos.Y + bugRadius), 2 * bugRadius, 2 * bugRadius);
+        foreach (Point[] obstacle in obstaclesUI)
+            e.Graphics.DrawLines(Pens.Black, obstacle);
+        e.Graphics.DrawLine(Pens.Blue, new Point(X(0), Y(0)), new Point(X(500), Y(0)));
+        e.Graphics.DrawLine(Pens.Blue, new Point(X(500), Y(0)), new Point(X(500), Y(500)));
+        e.Graphics.DrawLine(Pens.Blue, new Point(X(500), Y(500)), new Point(X(0), Y(500)));
+        e.Graphics.DrawLine(Pens.Blue, new Point(X(0), Y(500)), new Point(X(0), Y(0)));
     }
 
     private void newObj_Click(object sender, EventArgs e)
     {
+        if (this.clickMode == (int)clickType.OBS)
+            this.clickMode = (int)clickType.NONE;
         this.clickMode = (int)clickType.OBS;
+    }
+    
+    private void startPos_Click(object sender, EventArgs e)
+    {
+        if(this.clickMode == (int)clickType.INITIAL)
+            this.clickMode = (int)clickType.NONE;
+        this.clickMode = (int)clickType.INITIAL;
+    }
+    
+    private void endPos_Click(object sender, EventArgs e)
+    {
+        if (this.clickMode == (int)clickType.FINAL)
+            this.clickMode = (int)clickType.NONE;
+        this.clickMode = (int)clickType.FINAL;
     }
 
     private void clickFrame_Click(object sender, EventArgs e)
